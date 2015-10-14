13dedd33117f82c5617ac1d7ec2aed5fe42f8d5f
Nacho Coloma <icoloma@gmail.com>
2015-10-14 13:53:58 +0200

    Bugfix: remove talk hash from URL when closing popup

--- build/koliseo-agenda.js
+++ build/koliseo-agenda.js
@@ -539,7 +539,10 @@ var AgendaView = (function () {
       var $selected = document.querySelector('.ka-table-td.selected');
       $selected && $selected.classList.remove('selected');
       var $details = document.querySelector('.ka-talk-details-window');
-      $details && $details.parentNode.removeChild($details);
+      if ($details) {
+        $details.parentNode.removeChild($details);
+        this.pushState(this.models[this.selectedDayId].name, this.selectedDayId);
+      }
       document.querySelector('.ka-overlay').classList.add('ka-hidden');
     }
   }, {
